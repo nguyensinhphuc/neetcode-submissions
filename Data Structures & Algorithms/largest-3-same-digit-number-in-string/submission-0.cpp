156318fa632fafffe82392acd5f32fc40ca247f2
PhucNguyen <nguyensinhphuc13052013@gmail.com>
2026-08-28 08:07:07 +0700

    Add: largest-3-same-digit-number-in-string - submission-0

---
+++ Data Structures & Algorithms/largest-3-same-digit-number-in-string/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+
+        for (int i = 0; i < num.size() - 2; i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                string current = num.substr(i, 3);
+
+                if (current > ans) {
+                    ans = current;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
